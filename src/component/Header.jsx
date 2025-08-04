33c0df2d6d93281a3448016d5b5e8a84ff7f747a
sajalparua <sajalparua.work@gmail.com>
2025-08-04 09:53:06 +0530

    update

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,48 +1,68 @@
-
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { ThemeContext } from '../context/ThemeContext';
 
 const Header = ({ onToggleSidebar, showSidebarToggle }) => {
   const { theme, switchTheme, themes } = useContext(ThemeContext);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setMenuOpen((prev) => !prev);
+  };
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top shadow">
       <div className="container-fluid">
-        {/* Toggle Sidebar Button (only for Theme 2) */}
+
+        {/* Sidebar Toggle Button (Theme 2 Only) */}
         {showSidebarToggle && (
           <button className="btn btn-outline-light me-2" onClick={onToggleSidebar}>
             ☰
           </button>
         )}
 
-        {/* Logo */}
+        {/* App Logo */}
         <Link className="navbar-brand fw-bold" to="/">
           🌈 HipsterApp
         </Link>
 
-        {/* Nav menu */}
-       
-        <div className="collapse navbar-collapse" id="navbarNav">
-           {
-          theme !== 'theme2' && (
-              <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/">Home</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/about">About</NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to="/contact">Contact</NavLink>
-            </li>
-          </ul>)
-        }
-        
+        {/* Mobile Collapse Toggle (for Theme 1 and 3) */}
+        {theme !== 'theme2' && (
+          <button
+            className="navbar-toggler"
+            type="button"
+            onClick={handleMenuToggle}
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        )}
+
+        {/* Collapse Menu */}
+        <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarNav">
+          {theme !== 'theme2' && (
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/" onClick={() => setMenuOpen(false)}>
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/about" onClick={() => setMenuOpen(false)}>
+                  About
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/contact" onClick={() => setMenuOpen(false)}>
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          )}
 
           {/* Theme Switcher */}
           <select
-            className="form-select w-auto ms-auto"
+            className="form-select w-auto ms-auto mt-2 mt-lg-0"
             value={theme}
             onChange={(e) => switchTheme(e.target.value)}
           >
@@ -59,8 +79,3 @@ const Header = ({ onToggleSidebar, showSidebarToggle }) => {
 };
 
 export default Header;
-
-
-
-
-
